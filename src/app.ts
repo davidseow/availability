e2d67a06c2432e48f6d26af1d49ee6c382ab40fa
David Seow <david.seow@gmail.com>
2022-09-21 14:30:49 +0100

    chore: restructure, move router to its own file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { json, urlencoded } from 'body-parser';
 
-import { Event } from './types';
+import eventRouter from './event.router';
 
 dotenv.config();
 
@@ -14,40 +14,6 @@ app.use(urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
   res.send('👋 Hi there!');
 });
-
-app.post('/event', (req: Request, res: Response) => {
-  const { name, description, timeSlots, participants, duration } = req.body;
-
-  const response: Event = {
-    id: 'b183d4e4-c32e-4121-b6dd-b046c117058a',
-    name,
-    description,
-    timeSlots,
-    participants,
-    duration
-  };
-
-  res.status(200).json(response);
-});
-
-app.put('/event/:id', (req: Request, res: Response) => {
-  res.send({
-    message: 'updated',
-    participant: 'Dave',
-    selectedTimeSlots: [1664654400000]
-  });
-});
-
-app.get('/event/:id', (req: Request, res: Response) => {
-  res.send({
-    name: 'Poker night 🚬🃏',
-    description: 'Poker night, with fake dinero',
-    timeSlots: [1664654400000, 1664656200000],
-    duration: 3600,
-    selectedTimeSlots: [1664654400000, 1664654400000, 1664656200000],
-    participants: ['Dave', 'Steph'],
-    voted: ['Dave', 'Steph']
-  });
-});
+app.use('/event', eventRouter);
 
 export default app;
---
+++ src/event.router.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response } from 'express';
+import { Event } from './types';
+
+const eventRouter = Router();
+
+eventRouter.post('/', (req: Request, res: Response) => {
+  const { name, description, timeSlots, participants, duration } = req.body;
+
+  const response: Event = {
+    id: 'b183d4e4-c32e-4121-b6dd-b046c117058a',
+    name,
+    description,
+    timeSlots,
+    participants,
+    duration
+  };
+
+  res.status(200).json(response);
+});
+
+eventRouter.put('/:id', (req: Request, res: Response) => {
+  res.send({
+    message: 'updated',
+    participant: 'Dave',
+    selectedTimeSlots: [1664654400000]
+  });
+});
+
+eventRouter.get('/:id', (req: Request, res: Response) => {
+  res.send({
+    name: 'Poker night 🚬🃏',
+    description: 'Poker night, with fake dinero',
+    timeSlots: [1664654400000, 1664656200000],
+    duration: 3600,
+    selectedTimeSlots: [1664654400000, 1664654400000, 1664656200000],
+    participants: ['Dave', 'Steph'],
+    voted: ['Dave', 'Steph']
+  });
+});
+
+export default eventRouter;
