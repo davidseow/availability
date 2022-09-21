92151a597c6db12d9d1c0c6e1cc4548f3f5dbadc
David Seow <david.seow@gmail.com>
2022-09-21 08:50:51 +0100

    chore: restructure server script

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from './app';
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
